f2c887f1e7578d2b11271794b7bbc11b18af1b12
xiaotian.shi <54526322@qq.com>
2016-06-18 23:06:31 +0800

    添加opencv环境

--- auth/main/java/com/sxt/auth/base/BaseAuthController.java
+++ auth/main/java/com/sxt/auth/base/BaseAuthController.java
@@ -3,6 +3,7 @@ package com.sxt.auth.base;
 import com.jfinal.core.Controller;
 import com.jfinal.kit.JsonKit;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -29,5 +30,37 @@ public class BaseAuthController extends Controller {
 
         this.renderJson(resultMap);
 
+    }
+
+    /**
+     *
+     * 返回简单失败报文
+     *
+     * @param msg 失败信息
+     */
+    public void renderFailJson(String msg) {
+
+
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+        resultMap.put("code", 201);
+        resultMap.put("data", msg);
+
+        this.renderJson(resultMap);
+
+    }
+
+    /**
+     *
+     * 获取登录者ip
+     *
+     */
+    public String getIp() {
+
+        // 获取请求对象
+        HttpServletRequest request = this.getRequest();
+        // 获取ip
+        String ip = request.getRemoteAddr();
+
+        return ip;
     }
 }
---
+++ graphics/main/java/com/sxt/graphics/opencvMD.java
@@ -0,0 +1,18 @@
+package com.sxt.graphics;
+
+/**
+ *
+ * opencv 图形图像处理
+ *
+ * Created by sxt on 2016/5/15.
+ */
+public class OpenCvMD {
+
+    // 静态化
+    public static final OpenCvMD me = new OpenCvMD();
+
+    // 样例
+    public void demo() {
+
+    }
+}
